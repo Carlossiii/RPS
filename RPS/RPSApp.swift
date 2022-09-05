1adb21f03c4c012a9a58193d48feaf89a85146f0
Carlossiii <cv5673@gmail.com>
2022-09-05 19:15:14 -0300

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Carlos Vinicius on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
